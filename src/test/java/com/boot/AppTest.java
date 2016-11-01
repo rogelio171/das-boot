4e959469d4666df1c4f02d4b2e1171a28db8e3a6
Rogelio Orona Medina <rogelio@localhost.localdomain>
2016-10-31 22:35:58 -0600

    Add first test

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,38 +1,17 @@
 package com.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import com.boot.controller.HomeController;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import static org.junit.Assert.assertEquals;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+
+	@Test
+    public void testApp() {
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result, "Das Boot, reporting for duty!");
     }
 }
